cafdc65ee36a021f83d468016460a9856da5305b
Ademola Sobaki <demolasobaki@gmail.com>
2023-09-28 14:40:46 +0000

    #36 upload assets using for_each

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -35,7 +35,18 @@ resource "aws_s3_object" "index_html" {
     replace_triggered_by = [terraform_data.content_version.output]
     ignore_changes = [etag]
   }
+}
 
+resource "aws_s3_object" "upload_assets" {
+  for_each = fileset(var.assets_path,"*.{jpg,jpeg,png,gif}")
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "assets/${each.key}"
+  source = "${var.assets_path}/${each.key}"
+  etag = filemd5("${var.assets_path}/${each.key}")
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
